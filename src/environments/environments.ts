7ac0a2ffa90a6faaa72f9ce7e60823da85388253
Jan <jhondellugagay@gmail.com>
2025-12-06 13:24:18 +0800

    Update frontend with latest changes and production build

--- src/environments/environments.ts
+++ src/environments/environments.ts
@@ -1,9 +1,4 @@
-// export const environment = {
-//   production: false,
-//   apiUrl: 'http://localhost:5000' // your local backend URL
-// };
-
 export const environment = {
-  production: true,
-  apiUrl: 'https://attendance-backend-production-2893.up.railway.app'
+  production: false,
+  apiUrl: 'http://localhost:5000' // your local backend URL
 };
